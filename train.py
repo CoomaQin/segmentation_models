6483a275dfd1ab95b131a5edb67b021421bf9867
Huixiong Qin <coomafine@gmail.com>
2024-09-23 00:24:58 -0400

    adapt to area seg

---
+++ train.py
@@ -0,0 +1,304 @@
+import os
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset as BaseDataset
+import numpy as np
+import cv2
+import pytorch_lightning as pl
+import segmentation_models_pytorch as smp
+import torch
+from torch.optim import lr_scheduler
+import albumentations as A
+
+
+class Dataset(BaseDataset):
+    AREA_CLASSES = [
+        "unlabelled",
+        "restricted",
+        "ballast",
+        "track"
+    ]
+
+    OBJ_CLASSES = [
+        "unlabelled",
+        "person"
+    ]
+
+    def __init__(self, images_dir, masks_dir, height=640, width=640, classes=None, augmentation=None):
+        self.ids = os.listdir(images_dir)
+        self.images_fps = [os.path.join(images_dir, image_id) for image_id in self.ids]
+        self.masks_fps = [os.path.join(masks_dir, image_id[:-4] + ".png") for image_id in self.ids]
+        self.height, self.width = height, width
+
+        # Always map background ('unlabelled') to 0
+        self.background_class = self.AREA_CLASSES.index("unlabelled")
+
+        # If specific classes are provided, map them dynamically
+        if classes:
+            self.class_values = [self.AREA_CLASSES.index(cls.lower()) for cls in classes]
+        else:
+            self.class_values = list(range(len(self.AREA_CLASSES)))  # Default to all classes
+
+        # Create a remapping dictionary: class value in dataset -> new index (0, 1, 2, ...)
+        # Background will always be 0, other classes will be remapped starting from 1.
+        self.class_map = {self.background_class: 0}
+        self.class_map.update(
+            {
+                v: i
+                for i, v in enumerate(self.class_values)
+            }
+        )
+
+        self.augmentation = augmentation
+
+    def __getitem__(self, i):
+        # Read the image
+        image = cv2.imread(self.images_fps[i])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+        image = cv2.resize(image, (self.height, self.width), interpolation=cv2.INTER_LANCZOS4)
+
+        # area
+        mask = cv2.imread(self.masks_fps[i])
+        area_mask = mask[:, :, 0]
+        obj_mask = mask[:, :, 0]
+        area_mask = cv2.resize(area_mask, (self.height, self.width), interpolation=cv2.INTER_NEAREST)
+        # Create a blank mask to remap the class values
+        mask_remap = np.zeros_like(area_mask)
+        # Remap the mask according to the dynamically created class map
+        for class_value, new_value in self.class_map.items():
+            mask_remap[area_mask == class_value] = new_value
+
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask_remap)
+            image, mask_remap = sample["image"], sample["mask"]
+        image = image.transpose(2, 0, 1)
+        return image, mask_remap
+
+    def __len__(self):
+        return len(self.ids)
+
+
+# training set images augmentation
+def get_training_augmentation():
+    train_transform = [
+        A.HorizontalFlip(p=0.5),
+        A.ShiftScaleRotate(
+            scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=1, border_mode=0
+        ),
+        A.PadIfNeeded(min_height=640, min_width=640, always_apply=True),
+        A.RandomCrop(height=640, width=640, always_apply=True),
+        A.GaussNoise(p=0.2),
+        A.Perspective(p=0.5),
+        A.OneOf(
+            [
+                A.CLAHE(p=1),
+                A.RandomBrightnessContrast(p=1),
+                A.RandomGamma(p=1),
+            ],
+            p=0.9,
+        ),
+        A.OneOf(
+            [
+                A.Sharpen(p=1),
+                A.Blur(blur_limit=3, p=1),
+                A.MotionBlur(blur_limit=3, p=1),
+            ],
+            p=0.9,
+        ),
+        A.OneOf(
+            [
+                A.RandomBrightnessContrast(p=1),
+                A.HueSaturationValue(p=1),
+            ],
+            p=0.9,
+        ),
+    ]
+    return A.Compose(train_transform)
+
+
+def get_validation_augmentation():
+    """Add paddings to make image shape divisible by 32"""
+    test_transform = [
+        A.PadIfNeeded(384, 480),
+    ]
+    return A.Compose(test_transform)
+
+
+class CamVidModel(pl.LightningModule):
+    def __init__(self, arch, encoder_name, in_channels, out_classes,  **kwargs):
+        super().__init__()
+        self.model = smp.create_model(
+            arch,
+            encoder_name=encoder_name,
+            in_channels=in_channels,
+            classes=out_classes,
+            **kwargs,
+        )
+
+        # Preprocessing parameters for image normalization
+        params = smp.encoders.get_preprocessing_params(encoder_name)
+        self.number_of_classes = out_classes
+        self.register_buffer("std", torch.tensor(params["std"]).view(1, 3, 1, 1))
+        self.register_buffer("mean", torch.tensor(params["mean"]).view(1, 3, 1, 1))
+
+        # Loss function for multi-class segmentation
+        self.loss_fn = smp.losses.DiceLoss(smp.losses.MULTICLASS_MODE, from_logits=True)
+
+        # Step metrics tracking
+        self.training_step_outputs = []
+        self.validation_step_outputs = []
+        self.test_step_outputs = []
+
+    def forward(self, image):
+        # Normalize image
+        image = (image - self.mean) / self.std
+        mask = self.model(image)
+        return mask
+
+    def shared_step(self, batch, stage):
+        image, mask = batch
+
+        # Ensure that image dimensions are correct
+        assert image.ndim == 4  # [batch_size, channels, H, W]
+
+        # Ensure the mask is a long (index) tensor
+        mask = mask.long()
+
+        # Mask shape
+        assert mask.ndim == 3  # [batch_size, H, W]
+
+        # Predict mask logits
+        logits_mask = self.forward(image)
+
+        assert (
+            logits_mask.shape[1] == self.number_of_classes
+        )  # [batch_size, number_of_classes, H, W]
+
+        # Ensure the logits mask is contiguous
+        logits_mask = logits_mask.contiguous()
+
+        # Compute loss using multi-class Dice loss (pass original mask, not one-hot encoded)
+        loss = self.loss_fn(logits_mask, mask)
+
+        # Apply softmax to get probabilities for multi-class segmentation
+        prob_mask = logits_mask.softmax(dim=1)
+
+        # Convert probabilities to predicted class labels
+        pred_mask = prob_mask.argmax(dim=1)
+
+        # Compute true positives, false positives, false negatives, and true negatives
+        tp, fp, fn, tn = smp.metrics.get_stats(
+            pred_mask, mask, mode="multiclass", num_classes=self.number_of_classes
+        )
+
+        return {
+            "loss": loss,
+            "tp": tp,
+            "fp": fp,
+            "fn": fn,
+            "tn": tn,
+        }
+
+    def shared_epoch_end(self, outputs, stage):
+        # Aggregate step metrics
+        tp = torch.cat([x["tp"] for x in outputs])
+        fp = torch.cat([x["fp"] for x in outputs])
+        fn = torch.cat([x["fn"] for x in outputs])
+        tn = torch.cat([x["tn"] for x in outputs])
+
+        # Per-image IoU and dataset IoU calculations
+        per_image_iou = smp.metrics.iou_score(
+            tp, fp, fn, tn, reduction="micro-imagewise"
+        )
+        dataset_iou = smp.metrics.iou_score(tp, fp, fn, tn, reduction="micro")
+
+        metrics = {
+            f"{stage}_per_image_iou": per_image_iou,
+            f"{stage}_dataset_iou": dataset_iou,
+        }
+
+        self.log_dict(metrics, prog_bar=True)
+
+    def training_step(self, batch, batch_idx):
+        train_loss_info = self.shared_step(batch, "train")
+        self.training_step_outputs.append(train_loss_info)
+        return train_loss_info
+
+    def on_train_epoch_end(self):
+        self.shared_epoch_end(self.training_step_outputs, "train")
+        self.training_step_outputs.clear()
+
+    def validation_step(self, batch, batch_idx):
+        valid_loss_info = self.shared_step(batch, "valid")
+        self.validation_step_outputs.append(valid_loss_info)
+        return valid_loss_info
+
+    def on_validation_epoch_end(self):
+        self.shared_epoch_end(self.validation_step_outputs, "valid")
+        self.validation_step_outputs.clear()
+
+    def test_step(self, batch, batch_idx):
+        test_loss_info = self.shared_step(batch, "test")
+        self.test_step_outputs.append(test_loss_info)
+        return test_loss_info
+
+    def on_test_epoch_end(self):
+        self.shared_epoch_end(self.test_step_outputs, "test")
+        self.test_step_outputs.clear()
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=2e-4)
+        scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=50, eta_min=1e-5)
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "interval": "step",
+                "frequency": 1,
+            },
+        }
+
+
+if __name__ == "__main__":
+    DATA_DIR = "/home/hq63/segmentation/dataset/merge"
+
+    x_train_dir = os.path.join(DATA_DIR, "train", "images")
+    y_train_dir = os.path.join(DATA_DIR, "train", "masks")
+
+    x_valid_dir = os.path.join(DATA_DIR, "val", "images")
+    y_valid_dir = os.path.join(DATA_DIR, "val", "masks")
+
+    train_dataset = Dataset(
+        x_train_dir,
+        y_train_dir,
+        augmentation=get_training_augmentation(),
+    )
+    image, mask = train_dataset[0]
+    valid_dataset = Dataset(
+        x_valid_dir,
+        y_valid_dir,
+        augmentation=get_validation_augmentation(),
+    )
+    # Change to > 0 if not on Windows machine
+    train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=0)
+    valid_loader = DataLoader(valid_dataset, batch_size=8, shuffle=False, num_workers=0)
+
+    # Some training hyperparameters
+    EPOCHS = 50
+    T_MAX = EPOCHS * len(train_loader)
+    # Always include the background as a class
+    OUT_CLASSES = len(train_dataset.AREA_CLASSES)
+
+    model = CamVidModel("FPN", "resnext50_32x4d", in_channels=3, out_classes=OUT_CLASSES)
+
+    trainer = pl.Trainer(max_epochs=EPOCHS, log_every_n_steps=1)
+
+    trainer.fit(
+        model,
+        train_dataloaders=train_loader,
+        val_dataloaders=valid_loader,
+    )
+
+    # run validation dataset
+    valid_metrics = trainer.validate(model, dataloaders=valid_loader, verbose=False)
+    print(valid_metrics)
